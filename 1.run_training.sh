3e05771383610ad58d5d391e1824918ef80ed8d2
Tan Khanh Nguyen <63031249+NguyenTanKhanh@users.noreply.github.com>
2025-05-15 17:06:35 +0200

    Update 1.run_training.sh

--- 1.run_training.sh
+++ 1.run_training.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Activate your environment if needed
-# source ~/Downloads/yes/envs/DrugEx/bin/activate
-
-# Run the training script with specified arguments
 python train_drugex_graph.py \
   --gpu 0 \
-  --frags c1cnccn1 c1cnccn1.c1ccsc1 \
+  --frag_file "frags_enamine.smi" \
   --graph_input_folder datasets/encoded/graph \
-  --batch_size 4 \
-  --epochs 2 \
-  --n_samples 100
+  --batch_size 128 \
+  --epochs 100 \
+  --n_samples 200 \
+  --min_sa 0.8
+
+
+
+
