1e96b9313f745ea169d7ee4fc2882c6e29bad177
Tan Khanh Nguyen <63031249+NguyenTanKhanh@users.noreply.github.com>
2025-05-15 17:08:00 +0200

    Update 3.run_vinascreen.sh

--- 3.run_vinascreen.sh
+++ 3.run_vinascreen.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Optional: Make sure the vina binary is executable
+chmod +x vina_1.2.7_linux_x86_64
+
+# Run the Vina docking screen
 python vinascreen.py \
-  --receptor /home/khanhnt/Desktop/Hit_to_lead/data/Docking/protonation.pdbqt\
-  --ligand_dir /home/khanhnt/Desktop/Hit_to_lead/lead_generation_output/pdbqt_files\
+  --receptor data/Docking/protonation.pdbqt \
+  --ligand_dir lead_generation_output/pdbqt_files \
   --output_dir docking_results \
   --center 4 50 37 \
   --size 45 45 45 \
-  --exhaustiveness 8
+  --exhaustiveness 32
